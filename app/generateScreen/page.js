6bfba8fa5e84be77e99b76fb967b15e75d522700
Ian Lopes <160206746+IAN08527@users.noreply.github.com>
2025-08-22 14:57:15 +0530

    Add files via upload

---
+++ app/generateScreen/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProcessingDialog from '@/app/components/ProcessingDialog';
+
+const Page = () => {
+  return (
+    <main className='bg-black h-[calc(100vh-9vh)] w-screen'>
+        <ProcessingDialog />
+    </main>
+  )
+}
+
+export default Page
