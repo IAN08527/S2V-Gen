9f2071cfc10b1448a02d0168949a520104f92b8a
Ian Lopes <160206746+IAN08527@users.noreply.github.com>
2025-08-22 14:42:44 +0530

    Add files via upload

--- app/account/page.js
+++ app/account/page.js
@@ -1,1 +1,11 @@
+import React from 'react'
 
+const page = () => {
+  return (
+    <div>
+      This hadles the account page where users can manage their profile, settings, and other account-related features. or login
+    </div>
+  )
+}
+
+export default page
