1a0bebae4db4bc3f3a0e48e2ea51d7cb473c2e30
Ian Lopes <160206746+IAN08527@users.noreply.github.com>
2025-08-22 14:52:01 +0530

    Add files via upload

--- app/api/process-text/route.js
+++ app/api/process-text/route.js
@@ -1,1 +1,27 @@
+"use server"
 
+import { NextResponse } from "next/server";
+import processScript from "@/src/processors/textProcessor.js";
+import fs from "fs/promises";
+import path from "path";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const script = body.script;
+
+    const options = {
+      sceneDuration: 6,
+      minSceneLength: 80,
+      maxSceneLength: 300,
+      wordsPerSecond: 2.5,
+    };
+
+    const processed_text = await processScript(script, options);
+
+    return NextResponse.json({ success: true, data: processed_text });
+  } catch (error) {
+    console.error("Error processing script:", error);
+    return NextResponse.json({ success: false });
+  }
+}
