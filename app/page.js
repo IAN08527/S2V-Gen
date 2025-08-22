2bd262e2568d30e8249dc1ddae7f225392e7fce6
Ian Lopes <160206746+IAN08527@users.noreply.github.com>
2025-08-22 14:41:38 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import InputBox from "./components/InputBox";
+
+const Page = () => {
+  return (
+    <>
+      <main className="bg-black h-[calc(100vh-9vh)] w-screen">
+        <InputBox />
+      </main>
+    </>
+  );
+};
+
+export default Page;
